49c73f059dbbcd7d8e17c72c55247947f9c77df1
ashleybisram <115745574+ashleybisram@users.noreply.github.com>
2024-12-19 12:14:06 -0500

    add custom.d.ts. edit tsconfig to include custom.d.ts to specify img type modules

---
+++ types/custom.d.ts
@@ -0,0 +1,21 @@
+declare module "*.jpg"{
+    // the value represents a file path
+    const value:string;
+    export = value;
+}
+declare module "*.png"{
+    const value:string;
+    export = value;
+}
+declare module "*.svg"{
+    const value:string;
+    export = value;
+}
+declare module "*.jpeg"{
+    const value:string;
+    export = value;
+}
+declare module "*.gif"{
+    const value:string;
+    export = value;
+}
